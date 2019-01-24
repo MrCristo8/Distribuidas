8d3e96ceef1269d7a2ee26f16266bdac92590234
wabrborich@hotmail.com <wabrborich@hotmail.com>
2019-01-24 14:55:10 -0500

    admin changes to display messages

--- Code/CR_WB_WebPage/web/js/admin.js
+++ Code/CR_WB_WebPage/web/js/admin.js
@@ -5,3 +5,55 @@
  */
 
 
+(function (window, document, JSON)
+{
+    'use strict';
+
+    var url = 'ws://' + window.location.host + '/CR_WB_WebPage/chat';
+    var ws = new WebSocket(url);
+    let msg_display = document.getElementById('msg_display');
+    var send_btn = document.getElementById('msg_btn');
+    let username = document.getElementById('user_name');
+    let message = document.getElementById('msg_txt');
+
+    ws.onopen = onOpen;
+    ws.onclose = onClose;
+    ws.onmessage = onMessage;
+
+    send_btn.addEventListener('click', send);
+
+    function onOpen()
+    {
+        console.log('Connected Client');
+    }
+
+    function onClose()
+    {
+        console.log('Disconected Client');
+    }
+
+    function send()
+    {
+        var msg = {
+            name: message.value.split(',')[0],
+            text: message.value.split(',')[1],
+            date: new Date().toLocaleString()
+        };
+
+        let display = username.value + ', ' + msg.date + ': ' + msg.text;
+
+        msg_display.innerHTML += '</br>' + display;
+
+        ws.send(JSON.stringify(msg));
+    }
+
+    function onMessage(event)
+    {
+        var obj = JSON.parse(event.data);
+        let msg = obj.name + ', ' + obj.date + ': ' + obj.text;
+
+        msg_display.innerHTML += '</br>' + msg;
+
+    }
+
+})(window, document, JSON);
